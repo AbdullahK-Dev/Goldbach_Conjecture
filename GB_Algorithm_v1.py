4569a39627de2575ef9168f774ad6b6f640d84ee
AbdullahK <AbdullahKhalil247@gmail.com>
2025-12-29 14:07:34 +0000

    fixed functions handling primes,gb nums and outputs

--- GB_Algorithm_v1.py
+++ GB_Algorithm_v1.py
@@ -1,49 +1,91 @@
 import math
 import numpy as np
 
-p_nums = np.array([False,False,True])
-gb_pairs = np.array([])
-p_nums_length = len(p_nums)
+#p stands for prime and gb stands for goldbach
+
+p_bool_array = np.array([False, False, True])#index position represents the number, value represents whether its prime
+p_bool_length = len(p_bool_array)
+p_nums = np.where(p_bool_array == True)#holds the actual numbers
+gb_pairs = []
 
 
+def make_multiples_false(num):#this acts on p_bool_array only
+    global p_bool_array
 
-def make_multiples_false(num):
-    global p_nums
-
-    start = num+1 # +1 ensures it does not include the num in the iteration
-    for possible_multiple in range(start,p_nums_length):
-
+    for possible_multiple in range(num + 1, p_bool_length):# num + 1 ensures it does not include the num in the iteration
         if possible_multiple % num == 0:
-            p_nums[possible_multiple] = False
-
+            p_bool_array[possible_multiple] = False
 
 
 
 FIRST_PRIME = 2
-def find_primes_until(num):
-    global p_nums_length
+def find_primes_until(num):#primes found are stored in p_nums
+    global p_bool_length
+    global p_bool_array
     global p_nums
 
-    difference = num - p_nums_length
+    difference = num - p_bool_length
 
-    if difference < 0:
+    if difference < 0:#incase primes have already been foundd
         return
+    
+    true_arr = np.ones(difference, dtype=bool)#array that holds extra true values required
+    p_bool_array = np.concatenate((p_bool_array, true_arr))
+    p_bool_length = len(p_bool_array)
 
-    for counter in range(difference+1):#adds more True values until the list reaches the "limit" index number
-        p_nums = np.append(p_nums,True)
-    p_nums_length = len(p_nums)
 
-    limit = int(math.sqrt(p_nums_length)) + 1 # most efficent method as small primes would have marked all multiples
+
+    limit = int(math.sqrt(p_bool_length)) + 1 # most efficent method as small primes would have marked all multiples
     for current_num in range(FIRST_PRIME, limit):# searches for primes then makes their multiples False
-        prime =p_nums[current_num]
-
+        
+        prime = p_bool_array[current_num]
         if  prime == True:
             make_multiples_false(current_num)
 
+    p_nums = np.where(p_bool_array == True)
 
 
-def is_prime(num):
-    if num >= p_nums_length:
+
+def is_prime(num):#returns true or false
+    if num >= p_bool_length:
         find_primes_until(num)
 
-    return p_nums[num]
+    return p_bool_array[num]
+
+
+
+FIRST_EVEN = 4
+def find_gb_pairs_until(last_even):
+
+    find_primes_until(last_even)#to avoid calling the array multiple times
+    
+    for even_num in range(FIRST_EVEN, last_even+2, 2):# +2 because it is exclusive of last_even
+        
+        pair_found = False
+        counter = 0
+
+        while pair_found == False:
+
+            certain_p = p_nums[0][counter]
+            possible_p = even_num - certain_p
+            
+            if is_prime(possible_p) == True:
+                pair_found = True
+                gb_pairs.append([even_num, certain_p, possible_p])
+
+            counter += 1
+
+
+
+def output_gb_pairs():
+    for element in gb_pairs:
+        #format1 = f"{element[1]} + {element[2]} = {element[0]}".center(30)
+        format2 = f"{element[1]}".center(12) + f"{element[2]}".center(12) + "=" + f"{element[0]}".center(12)
+        print(format2)
+
+
+
+last_even = int(input("Up to what even number would you like to search until : "))
+
+find_gb_pairs_until(last_even)
+output_gb_pairs()
